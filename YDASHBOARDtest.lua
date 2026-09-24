2c20b4254299619ef8feb15ec04be4856460a449
princediaz111222 <princediaz111222@gmail.com>
2026-09-24 17:30:46 +0800

    Update YDASHBOARDtest.lua

--- YDASHBOARDtest.lua
+++ YDASHBOARDtest.lua
@@ -956,7 +956,7 @@ end)
 
 Connect (ExecuteButton2.MouseButton1Click, function()
 	Notify("Execute", "SAEV4 button clicked")
-https://api.luarmor.net/files/v4/loaders/4595fe31a5f7a8b4f4dd7071f3119ef7.lua
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/4595fe31a5f7a8b4f4dd7071f3119ef7.lua"))()
 end)
 
 
